5d5beabc4bd3e23f85daa12205f72e62e19780d5
chrstnglmtc <christinegulmatico@gmail.com>
2024-03-15 14:12:04 +0800

    base files and login card

---
+++ usermgmt/src/components/Login/Login.tsx
@@ -0,0 +1,11 @@
+import LoginCard from "./LoginCard";
+
+const Login = () => {
+    return (
+        <div className="container max-w-screen-lg mx-auto flex justify-center items-center h-screen">
+            <LoginCard />
+        </div>
+    )
+}
+
+export default Login;
